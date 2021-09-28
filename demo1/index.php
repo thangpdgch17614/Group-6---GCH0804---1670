9cd4e4caa4cf01663473de686dc18ce369ea16a5
Ngô Văn Đức <63224131+NerDucc@users.noreply.github.com>
2021-09-28 07:17:57 +0700

    Add files via upload
    Login form for admin

---
+++ demo1/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+	    <script>
+      $(document).ready(function(){
+        $("#show").click(function(){
+          $("#design").show();
+            });
+          $("#closeFrameDesign").click(function(){
+          $("#design").hide();
+        });
+      });
+    </script>
+</head>
+<body>
+<div class="header">
+  <img src="img/baner.jpg" width="100%"></div>
+</div>
+
+<div class="topnav">
+  <a href="#">Home</a>
+  <a href="view/signin.php">Sign in</a>
+</div>
+</div>
+      
+    </div>
+  </div>
+</div>
+</body>
+</html>
